62bc07cb3a1662ab0b2b56bbf0fccb7518ba7cc4
Michael Jones <michael.daniel.jones@gmail.com>
2008-11-03 21:03:45 +0000

    first checkin

---
+++ jquery.carousel.js
@@ -0,0 +1,55 @@
+(function($){
+  $.fn.carousel = function(o) {
+    return this.each(function() {
+      $.carousel(this, o);
+    });
+  };
+
+  $.carousel = function(element, settings) {
+    settings = $.extend({
+       index: 1,
+    }, settings);
+    this.index = 1;
+    this.locked = 1;
+    
+    children = $(element).children();
+    this.itemWidth = $(children[0]).width();
+    totalSize = children.length * this.itemWidth;
+    
+    $(element).css("width", totalSize)
+    
+    this.list = $(element);
+    this.container = this.list.wrap("<div></div>").parent();
+    this.container.css("width", this.itemWidth);
+    
+    self = this;
+    $(settings.nextButton).bind("click", function(e) {
+      self.list.animate({left: '-=200px'}, "swing");
+    });
+    $(settings.prevButton).bind("click", function(e) {
+      self.list.animate({left: '+=200px'}, "swing");
+    });
+  };
+  
+  $.fn.extend({
+    next: function() {
+      if(this.lock)
+        return true;
+      this.lock = true;
+      this.index++;
+      // if(this.index > this.size()) {
+      //   $('#scroll').append("<div style='background-color:blue'>two</div>");
+      //   $('#scroll').width($('#scroll').width() + 200);
+      // }
+      this.list.animate({left: '-=200px'}, 400, "swing", function() {
+        this.lock=false;
+      });
+    }
+  });
+  
+  // $.carousel.fn.extend({
+  //   
+  // });
+  
+})(jQuery);
+
