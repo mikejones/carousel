75504b027edde978148412682ba495bbc97c55b3
Michael Jones <michael.daniel.jones@gmail.com>
2008-11-06 18:55:49 +0000

    tidied up the code slightly

--- jquery.carousel.js
+++ jquery.carousel.js
@@ -7,30 +7,37 @@
     return this.each(function() {
       var list = $(this);
       var listItemWidth = $(list.children()[0]).width();
-      list.css("width", list.children().length * listItemWidth);
-      
-      var container = container = list.wrap("<div></div>").parent().addClass("carousel").css("width", 200 );
-      
-      var index = 0;
       var lock = false;
       
-      $(options.nextButton).bind("click", function(e) {
-        if(!lock && index < (list.children().length-1)) {
-          lock=true;
-          index++;
-          list.animate({left: '-='+listItemWidth+'px'}, 400, "swing", function() { lock=false;});
-        }
-        return false;
-      });
+      list.css("width", list.children().length * listItemWidth);
+      var container = container = list.wrap("<div></div>").parent().addClass("carousel").css("width", 200 );
       
-      $(options.prevButton).bind("click", function(e) {
+      $(options.nextButton).bind("click", nextButtonClick);
+      $(options.prevButton).bind("click", previousButtonClick);
+      
+      function removeLock() { lock=false };
+      
+      function previousButtonClick(e) {
         if(!lock && index > 0) {
           lock=true;
           index--;
-          list.animate({left: '+='+listItemWidth+'px'}, 400, "swing", function() { lock=false;});
+          list.animate({left: '+='+listItemWidth+'px'}, 400, "swing", removeLock);
         }
         return false;
-      });
+      }
+      
+      function nextButtonClick(e) {
+        if(!lock) {
+          lock=true;
+          if (index == (list.children().length-1)) {
+            list.append(options.nextMissingItem(index));
+            list.css("width", list.children().length * listItemWidth);
+          }
+          index++;
+          list.animate({left: '-='+listItemWidth+'px'}, 400, "swing", removeLock);
+        }
+        return false;
+      }
     });
   };
 })(jQuery);
