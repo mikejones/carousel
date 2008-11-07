fb13d2135207a2822d1ed96755b54bb2afa488c4
Michael Jones <michael.daniel.jones@gmail.com>
2008-11-07 10:01:38 +0000

    added some more documentation, tidied things up a little

---
+++ README.txt
@@ -0,0 +1,13 @@
+Carousel
+========
+
+Carousel is a jQuery plugin that provides a simple, basic carousel functionality for your website. It is in now way meant to be fully functional, but is lightweight and easily adaptable to add your own functionality.
+
+Requirement
+-----------
+
+
+Supported Browsers
+------------------
+
+ 
--- jquery.carousel.js
+++ jquery.carousel.js
@@ -7,7 +7,7 @@
     return this.each(function() {
       var lock = false;
       var list = $(this);
-
+      // setup
       var container = list.wrap("<div></div>").parent()
       var listItemWidth = $(list.children()[0]).width();
       list.css("width", list.children().length * listItemWidth);
@@ -17,7 +17,7 @@
       $(options.nextButton).bind("click", nextButtonClick);
       $(options.prevButton).bind("click", previousButtonClick);
       
-      function removeLock() { lock=false };
+      function removeLock() { lock=false; };
       
       function previousButtonClick(e) {
         if(!lock && options.index > 0) {
@@ -26,16 +26,16 @@
           list.animate({left: '+='+listItemWidth+'px'}, 400, "swing", removeLock);
         }
         return false;
-      }
+      };
       
       function nextButtonClick(e) {
         if(!lock && options.index < (list.children().length-1)) {
           lock=true;
           options.index++;
           list.animate({left: '-='+listItemWidth+'px'}, 400, "swing", removeLock);
-        }
+        } 
         return false;
-      }
+      };
     });
   };   
 })(jQuery);
